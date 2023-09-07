e192d4ab1f99d5cb31e5826f48b76a7cf5c8c9f5
Nicolas Souza Gomes <nicolas.gomes@imagemseguranca.com.br>
2023-09-07 00:34:38 -0300

    Create taskBar and styles #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import styles from './App.module.css';
 import Header from './components/Header';
+import TaskBar from './components/Taskbar';
 import './global.css';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+
+      <div className={styles.wrapper}>
+        <TaskBar />
+      </div>
+    </div>
   );
 }
 
